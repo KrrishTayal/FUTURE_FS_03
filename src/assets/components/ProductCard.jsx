c90a4aff9a1a5c1b2cb9d67afd25dd060c2648e7
Krrish Tayal <Krrish9tayal@gmail.com>
2025-06-26 14:12:36 +0530

    path changes1

--- src/assets/components/ProductCard.jsx
+++ src/assets/components/ProductCard.jsx
@@ -1,27 +1,4 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
 
-// function ProductCard({ product }) {
-//     return(
-//      <Link to= {`/product/${product.id}`}>
-//         <div className="shadow-lg rounded-md cursor-pointer">
-//             <img src={product.image} className="p-4" />
-//             <div className="bg-gray-50 p-4">
-//                 <h2 className="text-lg font-semibold my-4">{product.title.substring(0,20)+"..."}</h2>
-//                 <p className="text-sm text-zinc-500 border-b-2 pb-4"> {product.description.substring(0,75)+"..."} </p>
-//                 <div className="flex justify-between items-center mt-4">
-//                     <p className="text-xl font-semibold"> ${product.price}</p>
-//                      <p>View Details</p>
-                    
-//                 </div>
-//             </div>
-//         </div>
-//      </Link>
-//     );
-
-// }
-
-// export default ProductCard;
 
 
 import React from "react";
